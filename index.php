f02798bad0117253a86113767c49668f57980a9b
Claudio <criquelme@sbytech.cl>
2020-01-24 16:46:07 -0300

    test post

--- index.php
+++ index.php
@@ -1,31 +1,39 @@
 <?php
-$DBuser = "Claudio@mysqlmobilgeoapp";
-$DBpass = "Xxed9210";  
-$DBhost = "mysqlmobilgeoapp.mysql.database.azure.com";
-$DBname = "cpr52313_test_usr";
 
+$json = file_get_contents('php://input');
 
-try{
+if($json != null){
+      $data = json_decode($json);
+      print_r($data);
+      // $DBuser = "Claudio@mysqlmobilgeoapp";
+      // $DBpass = "Xxed9210";  
+      // $DBhost = "mysqlmobilgeoapp.mysql.database.azure.com";
+      // $DBname = "cpr52313_test_usr";
+      // try{
   
-    $DBcon = new PDO("mysql:host=$DBhost;dbname=$DBname",$DBuser,$DBpass);
-    $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-   }catch(PDOException $ex){
-    
-    die($ex->getMessage());
-   }
-$query = "SELECT * FROM usuarios";
- 
-$stmt = $DBcon->prepare($query);
-$stmt->execute();
+      //       $DBcon = new PDO("mysql:host=$DBhost;dbname=$DBname",$DBuser,$DBpass);
+      //       $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+      //      }catch(PDOException $ex){
+            
+      //       die($ex->getMessage());
+      //      }
+      //   $query = "SELECT * FROM usuarios";
+         
+      //   $stmt = $DBcon->prepare($query);
+      //   $stmt->execute();
+        
+      //   $userData = array();
+        
+      //   while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+          
+      //         $userData = $row;
+         
+      //   }
+        
+      //   echo json_encode($userData);
+        
 
-$userData = array();
-
-while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
-  
-      $userData = $row;
- 
 }
 
-echo json_encode($userData);
 
