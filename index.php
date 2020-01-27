ed1b073222924fd50e204b54da9568e41938ddc9
Claudio <criquelme@sbytech.cl>
2020-01-27 17:28:10 -0300

    sad

--- index.php
+++ index.php
@@ -2,37 +2,36 @@
 
 $usuario = $_POST["usuario"];
 $pass = $_POST["pass"];
-
 if($usuario != null){
     
-    echo $usuario;
+
       $DBuser = "Claudio@mysqlmobilgeoapp";
       $DBpass = "Xxed9210";  
       $DBhost = "mysqlmobilgeoapp.mysql.database.azure.com";
       $DBname = "cpr52313_test_usr";
-//       try{
+      try{
   
-//             $DBcon = new PDO("mysql:host=$DBhost;dbname=$DBname",$DBuser,$DBpass);
-//             $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $DBcon = new PDO("mysql:host=$DBhost;dbname=$DBname",$DBuser,$DBpass);
+            $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
-//            }catch(PDOException $ex){
+           }catch(PDOException $ex){
             
-//             die($ex->getMessage());
-//            }
-//         $query = "SELECT * FROM usuarios where usuario = '$usuario' and contrasena = '$pass'";
+            die($ex->getMessage());
+           }
+        $query = "SELECT * FROM usuarios where usuario = '$usuario' and contrasena = '$pass'";
          
-//         $stmt = $DBcon->prepare($query);
-//         $stmt->execute();
+        $stmt = $DBcon->prepare($query);
+        $stmt->execute();
         
    
         
-//         $count = mysqli_num_rows($stmt);
-// echo $count;
+        $count = mysqli_num_rows($stmt);
+        echo $count;
         
-//       //   if($userData => 0 ){
-//       //       echo true;
+      //   if($userData => 0 ){
+      //       echo true;
 
-//       //   }else{echo false;}
+      //   }else{echo false;}
         
         
 
