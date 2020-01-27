2b1da76efad0c64889de4c34bf5729b1eaee65cd
Claudio <criquelme@sbytech.cl>
2020-01-27 17:34:55 -0300

    asd

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
          
          $myusername = mysqli_real_escape_string($db,$_POST['usuario']);
          $mypassword = mysqli_real_escape_string($db,$_POST['pass']); 
-         
+         echo  $myusername;
          $sql = "SELECT id FROM admin WHERE usuario = '$myusername' and contrasena = '$mypassword'";
          $result = mysqli_query($db,$sql);
          $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
